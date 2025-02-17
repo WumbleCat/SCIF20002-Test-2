3ed748e461522e0bd49bf196ff4f2a765b7bdc5f
WumbleCat <komethtauch04@gmail.com>
2025-02-17 16:24:04 +0000

    implemented encapsulation and added comments for each function and parameter

--- include/disk.h
+++ include/disk.h
@@ -1,14 +1,43 @@
 #ifndef DISK_H
 #define DISK_H
 
+/**
+ * @brief This represents a moving disk in the simulation.
+ */
 class Disk {
+    private:
+        double x;  // x position of the disk
+        double y;   // y position of the disk
+        double radius;    // radius position of the disk
+    
     public:
-        double x;
-        double y;
-        double radius;
+        // Disk object constructor
         Disk(double x, double y, double r);
+
+        /**
+         * @brief Moves the disk by a given distance in x and y directions
+         * @param dx reprsents change in x position
+         * @param dy reprsents change in y position
+         */
         void move(double dx, double dy);
+
+        /**
+         * @brief Computes the Euclidean distance between this disk and another
+         * The formula is sqrt of [(x1 - x2)^2 + (y1 - y2)^2]
+         * @param other Pass by reference to another Disk object
+         * @return The Euclidean distance between the two disks
+         */
         double distance(Disk &d);   
+
+        // Getter functions for private variables
+        double getX();
+        double getY();
+        double getRadius();
+
+        // Setter functions
+        void setX(double newX);
+        void setY(double newY);
+        void setRadius(double newRadius);
 };
 
 #endif
--- src/disk.cpp
+++ src/disk.cpp
@@ -1,24 +1,44 @@
 #include "disk.h"
 #include <cmath>
 
+// Constructs a Disk object with a specified position and radius parameters
 Disk::Disk(double x, double y, double r){
     this->x = x;
     this->y = y;
     this->radius = r;
 }
 
+// Moves the disk by a given distance in x and y directions
 void Disk::move(double dx, double dy){
     this->x += dx;
     this->y += dy;
 }
 
+// Computes the Euclidean distance between this disk and another
 double Disk::distance(Disk& other){
-
-    // Calculate the Euclidean distance
-    // The formula is sqrt of [(x1 - x2)^2 + (y1 - y2)^2]
-
     double diff_x_squared = pow(this->x - other.x, 2);
     double diff_y_squared = pow(this->y - other.y, 2);
     return std::sqrt(diff_x_squared + diff_y_squared);
+}
 
+// Getter functions for private attributes.
+double Disk::getX(){ 
+    return x; 
+}
+double Disk::getY(){ 
+    return y; 
+}
+double Disk::getRadius(){ 
+    return radius; 
+}
+
+// Setter functions for private attributes.
+void Disk::setX(double newX){ 
+    x = newX; 
+}
+void Disk::setY(double newY) { 
+    y = newY; 
+}
+void Disk::setRadius(double newRadius) { 
+    radius = newRadius; 
 }
