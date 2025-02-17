1906b50b1fd3ff98af15e8150df6d0d993bc43a0
WumbleCat <komethtauch04@gmail.com>
2025-02-17 22:53:36 +0000

    Fixed view.py and view.ipynb to output the correct animation and save it as a gif file

--- scripts/view.py
+++ scripts/view.py
@@ -3,11 +3,27 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import glob
 from matplotlib.patches import Circle
+import os
 
-# Get all configurations from the confs folder
-files = glob.glob("confs/c*")
-# sort them in ascending order
-filenames = sorted(files, key=lambda x: int(x.split("/conf")[1]))
+# # Get all configurations from the confs folder
+# files = glob.glob("../confs/c*")
+# # sort them in ascending order
+# filenames = sorted(files, key=lambda x: int(x.split("\conf")[1]))
+# the lines above here have to be tinkered with since ipynb files handle paths differently from py files
+# its usually better to just locate the path and "add" it to your directory (more explained below)
+
+# Get the directory where the script is located (current directory)
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Build the absolute path to the confs folder (this assumes confs is one level up from scripts)
+confs_dir = os.path.join(script_dir, "..", "confs")
+
+# Use the absolute path in glob instead
+files = glob.glob(os.path.join(confs_dir, "conf*"))
+
+# Now run the original code given below
+
+filenames = sorted(files, key=lambda x: int(os.path.basename(x)[len("conf"):]))
 
 # Create a new figure
 fig, ax = plt.subplots()
@@ -33,5 +49,14 @@ def update(frame):
         patch.set_radius(r)
     return circles
 
-ani = FuncAnimation(fig, update, frames=len(filenames), blit=True)
+ani = FuncAnimation(fig, update, frames=len(filenames), blit=True, interval=50)
+
+# Define the output directory path like previously done and make sure it exists
+output_dir = os.path.join(script_dir, "..", "output")
+os.makedirs(output_dir, exist_ok=True)
+
+# Save animation inside the output directory
+gif_path = os.path.join(output_dir, "animation.gif")
+ani.save(gif_path, writer="pillow")
+
 plt.show()
