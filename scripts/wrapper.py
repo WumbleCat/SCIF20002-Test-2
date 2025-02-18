410b5b1478b41b9d9a4069d3c74d419efe84b1de
WumbleCat <komethtauch04@gmail.com>
2025-02-18 20:32:32 +0000

    added output folder back again

--- scripts/wrapper.py
+++ scripts/wrapper.py
@@ -5,7 +5,7 @@ import glob
 # create the class for the simulation
 class DiskSimulation:
     # initialize the object like how you'd call the exe file (like ./main <> <> <> <> <>) accurately
-    def __init__(self, N, displacement, radius, box_size, iterations, bin_dir="../bin"):
+    def __init__(self, N, displacement, radius, box_size, iterations):
         # connect the current working directory with the bin file 
         script_dir = os.path.dirname(os.path.abspath(__file__))
         self.executable = os.path.join(script_dir, "..", "bin", "main.exe")
